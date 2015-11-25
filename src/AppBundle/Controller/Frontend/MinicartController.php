85cf34e81dfcccaadf1f18986cc398fad887fa13
Yamir Ramirez <ysramirez@gmail.com>
2015-11-25 17:09:17 -0500

    mincart template

---
+++ src/AppBundle/Controller/Frontend/MinicartController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace AppBundle\Controller\Frontend;
+
+use AppBundle\Form\ProductType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class MinicartController extends Controller
+{
+	public function indexAction()
+	{
+//		$cart = $this->get('session')->has('cart') ? $this->get('session')->get('cart') : [];
+		return $this->render(
+			'AwFrontendTemplateBundle:Cart:index.html.twig'
+		);
+/*
+		return $this->render(
+			'AppBundle:app:index.html.twig',
+			[
+				'formsCart' => $this->getFormCart($cart)->createView(),
+				'formProducts' => $this->getFormProductsViews($this->getFormProducts($this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->findAll())),
+			]
+		);
+*/
+	}
+
+	public function addAction(Request $request, $id)
+	{
+		$product = $this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->find($id);
+
+		$form = $this->createForm(new ProductType(), $product);
+
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$cart = $this->get('session')->has('cart') ? $this->get('session')->get('cart') : [];
+			$cart[$product->getId()] = ['product' => $product, 'quantity' => $form->get('quantity')->getData()];
+			$this->get('session')->set('cart', $cart);
+		}
+		return $this->redirectToRoute('app_index');
+	}
+
+	public function checkoutAction()
+	{
+		// todo your checkout method
+	}
+
+	public function updatequantitycartAction(Request $request)
+	{
+		$cart = $this->get('session')->get('cart');
+
+		$formCart = $this->getFormCart($cart);
+
+		$formCart->handleRequest($request);
+
+		if ($formCart->isValid()) {
+			foreach ($formCart->getData() as $productId => $product) {
+				$cart [$productId]['quantity'] = $formCart->get($productId)->get('quantity')->getData();
+			}
+			$this->get('session')->set('cart', $cart);
+		}
+		return $this->redirectToRoute('app_index');
+	}
+
+	public function emptyCartAction()
+	{
+		$this->get('session')->remove('cart');
+
+		return $this->redirectToRoute('app_index');
+	}
+
+	private function getFormProducts($products)
+	{
+		$formProducts = [];
+		foreach ($products  as $key => $product) {
+			$formProducts[$key] = $this->createForm(new ProductType(), $product);
+		}
+
+		return $formProducts;
+	}
+
+	private function getFormProductsViews($formProducts)
+	{
+		$formProductsViews = [];
+		foreach ($formProducts  as $key => $form) {
+			$formProductsViews[$key] = $form->createView();
+		}
+		return $formProductsViews;
+	}
+
+	private function getFormCart($cart)
+	{
+		$builder = $this->createFormBuilder();
+
+		foreach ($cart as $productId => $cartProduct) {
+			$builder->add($productId, new ProductType($cartProduct['quantity']), ['data' => $cartProduct['product']]);
+		}
+
+		return $builder->getForm();
+	}
+}
