e66bd0be3e6782974ac802dff8eee9ea015212c8
Yamir Ramirez <ysramirez@gmail.com>
2016-02-21 10:33:36 -0500

    new paymentMethod

--- src/AppBundle/Controller/Backend/PaymentmethodController.php
+++ src/AppBundle/Controller/Backend/PaymentmethodController.php
@@ -3,16 +3,42 @@
 namespace AppBundle\Controller\Backend;
 
 
+use AppBundle\Entity\Paymentmethod;
+use AppBundle\Form\Backend\Paymentmethod\PaymentmethodType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PaymentmethodController extends Controller
 {
-  public function indexAction()
-  {
-    return $this->render('AwBackendTemplateBundle:Paymentmethod:index.html.twig', array(
-      'paymentmethods' => $this->getDoctrine()->getRepository('AppBundle:Paymentmethod')->findAll(),
-    ));
-  }
+    public function indexAction()
+    {
+        return $this->render('AwBackendTemplateBundle:Paymentmethod:index.html.twig', array(
+            'paymentmethods' => $this->getDoctrine()->getRepository('AppBundle:Paymentmethod')->findAll(),
+        ));
+    }
 
+    public function newAction()
+    {
+        $form = $this->createCreateForm($paymentMethod = new Paymentmethod());
 
+        return $this->render('AwBackendTemplateBundle:Hostingplan:new.html.twig', array(
+            'form'   => $form->createView(),
+            'entity' => $paymentMethod,
+        ));
+    }
+
+    private function createCreateForm(Paymentmethod $paymentMethod)
+    {
+        $form = $this->createForm(
+            new PaymentmethodType(),
+            $paymentMethod,
+            array(
+                'action' => $this->generateUrl('paymentmethod_create'),
+                'method' => 'POST',
+            )
+        );
+
+        $form->add('submit', 'submit', array('label' => 'Crear Metodo de Pago'));
+
+        return $form;
+    }
 }
